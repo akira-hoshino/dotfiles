812847c0a6ab167bcaf46849178c58ca62c68bb7
Hoshino <66937080+akira-hoshino@users.noreply.github.com>
2024-08-04 00:20:06 +0900

    fix: wezterm color

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,11 +7,45 @@ if wezterm.config_builder then
 end
 
 -- カラースキームの設定
-config.color_scheme = "AdventureTime"
+-- config.color_scheme = "Homebrew Light (Gogh)"
+config.color_scheme = "Tomorrow Night Eighties (Gogh)"
+-- config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "AdventureTime"
+-- config.color_scheme = "Chalk (dark) (terminal.sexy)"
 -- 背景透過
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.96
+config.macos_window_background_blur = 30
 -- フォントサイズの設定
-config.font_size = 20
+config.font_size = 24
+
+config.window_frame = {
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 16.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#333333",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#333333",
+}
+
+config.colors = {
+	tab_bar = {
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#575757",
+	},
+}
 
 -- ショートカットキー設定
 local act = wezterm.action
